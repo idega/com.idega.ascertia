b4c4400ba442810a2b6f09d56a65514af2c8893c
Juozas Zabuka <juozas@idega.com>
2008-09-26 10:00:42 +0000

    added new component

---
+++ src/java/com/idega/ascertia/presentation/AscertiaSigningForm.java
@@ -0,0 +1,18 @@
+package com.idega.ascertia.presentation;
+
+import java.rmi.RemoteException;
+
+import com.idega.presentation.Block;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Layer;
+
+public class AscertiaSigningForm extends Block{
+
+	public void main(IWContext iwc) throws RemoteException {
+		Layer div = new Layer();
+		
+		add(div);
+		
+		
+	}
+}
