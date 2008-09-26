bffdd8466b1a5cad4988754dd40346f717e172a0
Juozas Zabuka <juozas@idega.com>
2008-09-26 11:27:23 +0000

    added more constance

--- src/java/com/idega/ascertia/AscertiaConstants.java
+++ src/java/com/idega/ascertia/AscertiaConstants.java
@@ -2,6 +2,7 @@ package com.idega.ascertia;
 
 public class AscertiaConstants {
 
-	public static final String DOCUMENT_URL = "document_url";
+	public static final String UNSIGNED_DOCUMENT_URL = "unsigned_document_url";
+	public static final String SIGNED_DOCUMENT_URL = "signed_document_url";
 	
 }
