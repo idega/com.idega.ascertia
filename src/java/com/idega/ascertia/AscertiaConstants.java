11764213be10fc2dec62d1153d17570acfcce658
Juozas Zabuka <juozas@idega.com>
2008-09-30 12:00:10 +0000

    few more constants

--- src/java/com/idega/ascertia/AscertiaConstants.java
+++ src/java/com/idega/ascertia/AscertiaConstants.java
@@ -5,5 +5,9 @@ public class AscertiaConstants {
 	public static final String UNSIGNED_DOCUMENT_URL = "unsigned_document_url";
 	public static final String SIGNED_DOCUMENT_URL = "signed_document_url";
 	public static final String DOCUMENT_URL_ID = "document_url_id";
+	public static final String PARAM_TASK_ID = "taskId";
+	public static final String PARAM_VARIABLE_HASH = "variable_hash";
+	
+	
 	
 }
