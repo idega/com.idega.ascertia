4445130b5ef284f80ecab0ee833018c78daf34af
Juozas Zabuka <juozas@idega.com>
2009-02-03 13:05:07 +0000

    Added new method for process search, Changed the way pi's are found when attaching emails, added new field to CaseProcInstBind class

--- src/java/com/idega/ascertia/AscertiaServlet.java
+++ src/java/com/idega/ascertia/AscertiaServlet.java
@@ -15,7 +15,6 @@ import java.io.OutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
