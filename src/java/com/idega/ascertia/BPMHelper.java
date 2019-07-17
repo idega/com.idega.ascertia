a29a3b6c16d59871457762af18738e6f7d9c3c86
Valdas Žemaitis <valdas@idega.com>
2019-07-17 15:51:50 +0300

    IWContext must be provided

--- src/java/com/idega/ascertia/BPMHelper.java
+++ src/java/com/idega/ascertia/BPMHelper.java
@@ -24,50 +24,51 @@ import com.idega.jbpm.variables.BinaryVariable;
 import com.idega.jbpm.variables.VariablesHandler;
 import com.idega.jbpm.view.ViewSubmission;
 import com.idega.presentation.IWContext;
+import com.idega.util.CoreUtil;
 import com.idega.util.FileUtil;
 import com.idega.util.IOUtil;
 import com.idega.util.StringUtil;
 
 /**
  * Helper class to work with BPM in Ascertia module
- * 
+ *
  * @author juozas
  */
 @Service
 @Scope(BeanDefinition.SCOPE_SINGLETON)
 public class BPMHelper {
-	
+
 	private Logger logger = Logger.getLogger(BPMHelper.class.getName());
-	
+
 	@Autowired
 	private BPMFactory bpmFactory;
-	
+
 	@Autowired
 	private VariablesHandler variablesHandler;
-	
+
 	@Autowired
 	private FormConverterToPDF formConverterToPDF;
-	
+
 	public Map<String, Object> getVariables(Long taskInstanceId) {
 		return getVariablesHandler().populateVariables(taskInstanceId);
 	}
-	
+
 	AscertiaData saveSignedPDFAttachment(long taskInstanceId,
 	        Integer binaryVariableHash, byte[] signedPDF,
 	        String localizedPrefix, String signaturePlaceUsed,
 	        String signatureProfileUsed) throws Exception {
-		
+
 		TaskInstanceW taskInstance = getBpmFactory()
 		        .getProcessManagerByTaskInstanceId(taskInstanceId)
 		        .getTaskInstance(taskInstanceId);
-		
+
 		BinaryVariable binaryVariable = getBinaryVariable(taskInstanceId,
 		    binaryVariableHash);
-		
+
 		String fileName = binaryVariable.getFileName().replace(".pdf",
 		    "_signed.pdf");
 		InputStream inputStream = new ByteArrayInputStream(signedPDF);
-		
+
 		try {
 			String description;
 			if (localizedPrefix != null) {
@@ -79,16 +80,16 @@ public class BPMHelper {
 			        + (StringUtil.isEmpty(binaryVariable.getDescription()) ? binaryVariable
 			                .getFileName()
 			                : binaryVariable.getDescription());
-			
+
 			BinaryVariable signedBinaryVariable = taskInstance.addAttachment(
 			    binaryVariable.getVariable(), fileName, description,
 			    inputStream);
 			signedBinaryVariable.setMetadata(binaryVariable.getMetadata());
-			
+
 			signedBinaryVariable.getMetadata().put(
 			    AscertiaConstants.PARAM_ADD_EMPTY_SIGNATURES,
 			    Boolean.FALSE.toString());
-			
+
 			@SuppressWarnings("unchecked")
 			List<String> signaturePlacesUsed = signedBinaryVariable
 			        .getMetadata().get(
@@ -96,25 +97,25 @@ public class BPMHelper {
 			        .getMetadata().get(
 			            AscertiaConstants.PARAM_SIGNATURE_PLACES_USED)
 			        : new ArrayList<String>();
-			
+
 			signedBinaryVariable.getMetadata().put(
 			    AscertiaConstants.PARAM_SIGNATURE_PLACES_USED,
 			    signaturePlacesUsed);
-			
+
 			signedBinaryVariable.getMetadata().put(
 			    AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE,
 			    signatureProfileUsed);
-			
+
 			signaturePlacesUsed.add(signaturePlaceUsed);
 			signedBinaryVariable
 			        .setSigned(canBeSignedAgain(signedBinaryVariable));
 			signedBinaryVariable.update();
-			
+
 			binaryVariable.setHidden(true);
 			binaryVariable.update();
-			
+
 			VariablesHandler variablesHandler = getVariablesHandler();
-			
+
 			inputStream = variablesHandler.getBinaryVariablesHandler().getBinaryVariableContent(signedBinaryVariable);
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 //			byte buffer[] = new byte[1024];
@@ -139,40 +140,40 @@ public class BPMHelper {
 				inputStream = null;
 				return null;
 			}
-			
+
 			AscertiaData data = new AscertiaData();
 			data.setDocumentName(fileName);
 			data.setByteDocument(baos.toByteArray());
-			
+
 			return data;
-			
+
 		} catch (Exception e) {
 			logger.log(Level.SEVERE, "Unable to set binary variable with signed document for task instance: " + taskInstanceId, e);
 			throw new Exception(e);
 		}
 	}
-	
+
 	AscertiaData saveSignedPDFAsNewVariable(long taskInstanceId,
 	        byte[] signedPDF, String fileName, String signaturePlaceUsed,
 	        String signatureProfileUsed) throws Exception {
-		
+
 		TaskInstanceW taskInstance = getBpmFactory()
 		        .getProcessManagerByTaskInstanceId(taskInstanceId)
 		        .getTaskInstance(taskInstanceId);
-		
+
 		InputStream inputStream = new ByteArrayInputStream(signedPDF);
-		
+
 		try {
-			
+
 			Variable variable = Variable
 			        .parseDefaultStringRepresentation(AscertiaConstants.SIGNED_VARIABLE_NAME);
-			
+
 			BinaryVariable signedBinaryVariable = taskInstance.addAttachment(
 			    variable, fileName, fileName, inputStream);
-			
+
 			signedBinaryVariable.getMetadata().put(
 			    AscertiaConstants.PARAM_ADD_EMPTY_SIGNATURES, "false");
-			
+
 			@SuppressWarnings("unchecked")
 			List<String> signaturePlacesUsed = signedBinaryVariable
 			        .getMetadata().get(
@@ -180,22 +181,22 @@ public class BPMHelper {
 			        .getMetadata().get(
 			            AscertiaConstants.PARAM_SIGNATURE_PLACES_USED)
 			        : new ArrayList<String>();
-			
+
 			signaturePlacesUsed.add(signaturePlaceUsed);
 			signedBinaryVariable.getMetadata().put(
 			    AscertiaConstants.PARAM_SIGNATURE_PLACES_USED,
 			    signaturePlacesUsed);
-			
+
 			signedBinaryVariable.getMetadata().put(
 			    AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE,
 			    signatureProfileUsed);
-			
+
 			signedBinaryVariable
 			        .setSigned(canBeSignedAgain(signedBinaryVariable));
 			signedBinaryVariable.update();
-			
+
 			VariablesHandler variablesHandler = getVariablesHandler();
-			
+
 			inputStream = variablesHandler.getBinaryVariablesHandler().getBinaryVariableContent(signedBinaryVariable);
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 //			byte buffer[] = new byte[1024];
@@ -214,27 +215,27 @@ public class BPMHelper {
 				inputStream = null;
 				return null;
 			}
-			
+
 			ViewSubmission viewSubmission = getBpmFactory().getViewSubmission();
 			viewSubmission.setTaskInstanceId(taskInstance.getTaskInstanceId());
 			taskInstance.submit(viewSubmission);
-			
+
 			AscertiaData data = new AscertiaData();
 			data.setDocumentName(fileName);
 			data.setByteDocument(baos.toByteArray());
-			
+
 			return data;
-			
+
 		} catch (Exception e) {
 			logger.log(Level.SEVERE,
 			    "Unable to set binary variable with signed document for task instance: "
 			            + taskInstanceId, e);
 			throw new Exception(e);
-			
+
 		}
-		
+
 	}
-	
+
 	byte[] getDocumentInputStream(Integer variableHash, Long taskInstanceId) throws IOException {
 		BinaryVariable binaryVariable = getBinaryVariable(taskInstanceId, variableHash);
 		InputStream inputStream = getVariablesHandler()
@@ -252,76 +253,75 @@ public class BPMHelper {
 //		}
 		return baos.toByteArray();
 	}
-	
+
 	public BinaryVariable getBinaryVariable(long taskInstanceId,
 	        Integer binaryVariableHash) {
-		
+
 		List<BinaryVariable> variables = getVariablesHandler()
 		        .resolveBinaryVariables(taskInstanceId);
-		
+
 		for (BinaryVariable binaryVariable : variables) {
-			
+
 			if (binaryVariable.getHash().equals(binaryVariableHash)) {
-				
+
 				return binaryVariable;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public String getFileName(Integer variableHash, Long taskInstanceId) {
 		BinaryVariable binaryVariable = getBinaryVariable(taskInstanceId,
 		    variableHash);
 		return binaryVariable.getFileName();
 	}
-	
+
 	public String generateDocumentFromForm(Long taskInstanceId) {
-		
+		IWContext iwc = CoreUtil.getIWContext();
 		String variableHashString = getFormConverterToPDF()
 		        .getHashValueForGeneratedPDFFromXForm(
 		            String.valueOf(taskInstanceId),
 		            true,
-		            getBpmFactory().getTaskInstanceW(taskInstanceId).getName(
-		                IWContext.getCurrentInstance().getCurrentLocale()));
-		
+		            getBpmFactory().getTaskInstanceW(taskInstanceId).getName(iwc, iwc.getCurrentLocale()));
+
 		Integer binaryVariableHash = Integer.valueOf(variableHashString);
-		
+
 		Map<String, Object> variables = getVariables(taskInstanceId);
 		// If profile is set for signing generated pdf from task document, adding it to binary
 		// variables metadata
 		if (variables.get(AscertiaConstants.BPM_TASK_SIGNING_PROFILE) != null) {
-			
+
 			BinaryVariable binaryVariable = getBinaryVariable(taskInstanceId,
 			    binaryVariableHash);
-			
+
 			binaryVariable.getMetadata().put(
 			    AscertiaConstants.BPM_TASK_SIGNING_PROFILE,
 			    variables.get(AscertiaConstants.BPM_TASK_SIGNING_PROFILE));
 		}
-		
+
 		return variableHashString;
 	}
-	
+
 	public Map<String, String> getParametersForSigner(Long taskInstanceId,
 	        Integer variableHash) {
-		
+
 		Map<String, String> paramsMap = new HashMap<String, String>();
-		
+
 		BinaryVariable binaryVariable = getBinaryVariable(taskInstanceId,
 		    variableHash);
-		
+
 		// Adding a profile to use
 		if (binaryVariable.getMetadata().get(
 		    AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE) != null) {
-			
+
 			paramsMap.put(AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE,
 			    (String) binaryVariable.getMetadata().get(
 			        AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE));
 		} else {
 			Map<String, Object> variables = getVariables(taskInstanceId);
 			if (variables.get(AscertiaConstants.BPM_ATTACHMENT_SIGNING_PROFILE) != null) {
-				
+
 				paramsMap
 				        .put(
 				            AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE,
@@ -329,26 +329,26 @@ public class BPMHelper {
 				                    .get(AscertiaConstants.BPM_ATTACHMENT_SIGNING_PROFILE));
 			}
 		}
-		
+
 		// flag to add empty signatures filed(s) or not
 		if (binaryVariable.getMetadata().get(
 		    AscertiaConstants.PARAM_ADD_EMPTY_SIGNATURES) != null) {
-			
+
 			paramsMap.put(AscertiaConstants.PARAM_ADD_EMPTY_SIGNATURES,
 			    (String) binaryVariable.getMetadata().get(
 			        AscertiaConstants.PARAM_ADD_EMPTY_SIGNATURES));
-			
+
 		} else {
 			paramsMap.put(AscertiaConstants.PARAM_ADD_EMPTY_SIGNATURES,
 			    Boolean.TRUE.toString());
 		}
-		
+
 		// adding places for signatures
 		if (paramsMap.get(AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE) != null
 		        && paramsMap.get(
 		            AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE).equals(
 		            AscertiaConstants.PROP_EMPTY_ONE_SIGNATURE_PROFILE)) {
-			
+
 			paramsMap.put(AscertiaConstants.PARAM_SIGNATURE_PLACES_USED,
 			    AscertiaConstants.PROP_ONE_SIGNATURE_PROFILE_PLACE);
 		} else if (paramsMap
@@ -356,24 +356,24 @@ public class BPMHelper {
 		        && paramsMap.get(
 		            AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE).equals(
 		            AscertiaConstants.PROP_EMPTY_TWO_SIGNATURE_PROFILE)) {
-			
+
 			@SuppressWarnings("unchecked")
 			List<String> signingPlacesUsed = (List<String>) binaryVariable
 			        .getMetadata().get(
 			            AscertiaConstants.PARAM_SIGNATURE_PLACES_USED);
-			
+
 			StringBuilder optionsStringBuilder = new StringBuilder();
 			if (signingPlacesUsed != null) {
 				if (!signingPlacesUsed
 				        .contains(AscertiaConstants.PROP_TWO_SIGNATURE_PROFILE_LEFT)) {
-					
+
 					optionsStringBuilder
 					        .append(AscertiaConstants.PROP_TWO_SIGNATURE_PROFILE_LEFT);
 				}
-				
+
 				if (!signingPlacesUsed
 				        .contains(AscertiaConstants.PROP_TWO_SIGNATURE_PROFILE_RIGHT)) {
-					
+
 					optionsStringBuilder
 					        .append(AscertiaConstants.PROP_TWO_SIGNATURE_PROFILE_RIGHT);
 				}
@@ -386,16 +386,16 @@ public class BPMHelper {
 			paramsMap.put(AscertiaConstants.PARAM_SIGNATURE_PLACES_USED,
 			    optionsStringBuilder.toString());
 		}
-		
+
 		return paramsMap;
 	}
-	
+
 	protected boolean canBeSignedAgain(BinaryVariable binaryVariable) {
 		@SuppressWarnings("unchecked")
 		List<String> signingPlacesUsed = (List<String>) binaryVariable
 		        .getMetadata().get(
 		            AscertiaConstants.PARAM_SIGNATURE_PLACES_USED);
-		
+
 		if (binaryVariable.getMetadata().get(
 		    AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE) != null
 		        && binaryVariable.getMetadata().get(
@@ -404,7 +404,7 @@ public class BPMHelper {
 			if (signingPlacesUsed.size() > 1) {
 				return true;
 			}
-			
+
 		} else if (binaryVariable.getMetadata().get(
 		    AscertiaConstants.PARAM_SIGNATURE_PROFILE_TO_USE) != null
 		        && binaryVariable.getMetadata().get(
@@ -413,29 +413,29 @@ public class BPMHelper {
 			if (signingPlacesUsed.size() > 0) {
 				return true;
 			}
-			
+
 		}
 		return false;
 	}
-	
+
 	protected VariablesHandler getVariablesHandler() {
 		return variablesHandler;
 	}
-	
+
 	protected FormConverterToPDF getFormConverterToPDF() {
 		return formConverterToPDF;
 	}
-	
+
 	protected void setFormConverterToPDF(FormConverterToPDF formConverterToPDF) {
 		this.formConverterToPDF = formConverterToPDF;
 	}
-	
+
 	protected BPMFactory getBpmFactory() {
 		return bpmFactory;
 	}
-	
+
 	protected void setBpmFactory(BPMFactory bpmFactory) {
 		this.bpmFactory = bpmFactory;
 	}
-	
+
 }
